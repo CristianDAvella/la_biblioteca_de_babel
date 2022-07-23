f8ee28429a5b31dd57c07980002ad21ab2698932
unknown <cristianprietoavella@gmail.com>
2022-07-23 11:21:53 -0500

    Is Azure project now

---
+++ Escritor/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, Biblioteca: func.Out[str]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "All is good |^_^|",
+             status_code=200
+        )
