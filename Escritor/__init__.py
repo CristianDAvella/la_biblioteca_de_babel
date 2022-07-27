b5b6a44c12d8e48d0366e326e02b5a1af59a138f
unknown <cristianprietoavella@gmail.com>
2022-07-27 15:56:47 -0500

    Sending a string to blob

--- Escritor/__init__.py
+++ Escritor/__init__.py
@@ -1,11 +1,10 @@
-from asyncore import file_wrapper
 import logging
 import azure.functions as func
 
 
 def main(req: func.HttpRequest, Biblioteca: func.Out[str]) -> func.HttpResponse:
     #book = open("file.txt", "w", encoding="utf-8")
-    #book.write("Hello, It's me")
+    #book.write("Hello, It's me AGAIN")
     #book.close()
     Biblioteca.set("Hello, It's me")
     return func.HttpResponse("Function executed!!!")        
